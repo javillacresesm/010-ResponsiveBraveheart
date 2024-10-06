fee4efe3c844e1cf682b1797457f8a1ce90d5a29
JVillacreses <justlynxt@gmail.com>
2024-10-06 23:18:40 +0200

    .

--- app/src/main/java/com/example/a010_responsivebraveheart/MainActivity.java
+++ app/src/main/java/com/example/a010_responsivebraveheart/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a010_responsivebraveheart;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private Button btnE;
+    private Button btnF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +25,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        iniciarComponentes();
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Botón A", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void iniciarComponentes(){
+        btnA = (Button) findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        btnD = findViewById(R.id.btnD);
+        btnE = findViewById(R.id.btnE);
+        btnF = findViewById(R.id.btnF);
     }
 }
